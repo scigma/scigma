7ea15b3081c5a6e6a98d4851ea5ac907f8956bb3
Tom Bertalan <tom@tombertalan.com>
2018-03-26 11:21:18 -0400

    Add a run script and explicit windows guide.

---
+++ runscigma.py
@@ -0,0 +1,11 @@
+try:
+    import scigma
+except:
+    import traceback
+    traceback.print_exc()
+    
+    print()
+    print("Could not import scigma.")
+raw_input("Press enter to exit.")
+
+print("Exiting.")
