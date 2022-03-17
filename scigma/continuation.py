6ba8eb95813f6bbd4b277be14028452f875a7f85
rhoelzel <robert.hoelzel@gmx.de>
2022-03-17 02:30:06 +0100

    add rudimentary support for limit cycle continuation

--- scigma/continuation.py
+++ scigma/continuation.py
@@ -11,31 +11,132 @@ from . import picking
 from . import popups
 from . import equations
 
-AUTO_TYPES={1:'bp', 2:'lp',3:'hb', 0:'fp',10:'fp', -22:'cp', -32:'gh'}
-POINT_STYLES={1:gui.STAR,2:gui.PLUS,3:gui.CROSS,0:gui.RDOT,10:gui.RING, -22:gui.RPLUS, -32:gui.RCROSS}
+def auto_type(typ, periodic):
+    if typ == 0 and not periodic:
+        return 'fp' # steady state (start or end, stable)
+    if typ == 0 and periodic:
+        return 'lc' # limit cycle (continuation start or end, stable)
+    if typ == 10 and not periodic:
+        return 'fp' # steady state (continuation start or end, unstable)
+    if typ == 10 and periodic:
+        return 'lc' # limit cycle (continuation start or end, unstable)
+    if typ == 1:
+        return 'bp' # branch point of steady states
+    if typ == 2:
+        return 'lp' # limit point/saddle node of steady states (1p or start/end of 2p continuation)
+    if typ == 3:
+        return 'hb' # Hopf bifurcation (1p or start/end of 2p continuation)
+    if typ == 4 and not periodic:
+        return 'fp' # steady state (user defined)
+    if typ == 4 and periodic:
+        return 'lc' # limit cycle (user defined)
+    if typ ==-4 and not periodic:
+        return 'fp' # steady state (user defined)
+    if typ ==-4 and periodic:
+        return 'lc' # limit cycle (user defined)
+    if typ == 5:
+        return 'lclp' # limit point/saddle node of limit cycles
+    if typ == 6:
+        return 'lcbp' # branch point of limit cycles
+    if typ == 7:
+        return 'pd' # period doubling bifurcation
+    if typ == 8:
+        return 'tb' # torus bifurcation
+    if typ == -21:
+        return 'zh' # zero Hopf bifurcation (codimension 2)
+    if typ == -22:
+        return 'cp' # cusp (codimension 2)
+    if typ == -32:
+        return 'gh' # generalized Hopf bifurcation (codimension 2)
+
+    return '??'
+
+def point_style(autoType, stable):
+    if autoType == 'fp' and stable:
+        return gui.RDOT
+    if autoType == 'fp' and not stable:
+        return gui.RING
+    if autoType == 'lc' and stable:
+        return gui.QDOT
+    if autoType == 'lc' and not stable:
+        return gui.QUAD
+    if autoType == 'bp':
+        return gui.STAR
+    if autoType == 'lp':
+        return gui.PLUS
+    if autoType == 'hb':
+        return gui.CROSS
+    if autoType == 'lclp':
+        return gui.QPLUS
+    if autoType == 'lcbp':
+        return gui.STAR
+    if autoType == 'pd':
+        return gui.HASH
+    if autoType == 'tb':
+        return qui.QCROSS
+    if autoType == 'zh':
+        return gui.RPLUS
+    if autoType == 'cp':
+        return gui.CROSSHAIR
+    if autoType == 'gh':
+        return gui.RCROSS
+
+    return gui.STAR
+
 
 commands={}
 
-def prepare_continuation(g,parameters,blob,win):
+def prepare_continuation(g,parameters,blob,periodic,win):
     if win.eqsys.timestamp()!=g['timestamp']:
         raise Exception("structure of equation system has changed: cannot use "+g['identifier']+"for continuation")
 
     typ = 'fp' if 'autotype' not in g else g['autotype']
     lab = 0 if 'autolabel' not in g else g['autolabel']
+    periodicIn = False if 'periodic' not in g else g['periodic']
+
+    if periodicIn and not periodic:
+        raise Exception("use 'cycle' for continuation of periodic solutions")
 
     blob.set("IRS",lab)
 
-    if typ == 'fp':
+    if not periodic:
+        if typ == 'fp':
+            if len(parameters) != 1:
+                raise Exception("need exactly one parameter for steady state continuation")
+            blob.set("advanced AUTO.ISW",1)
+            blob.set("advanced AUTO.ILP",1)
+            blob.set("advanced AUTO.IPS",1)
+        elif typ in ['lp','hb']:
+            if len(parameters) == 1:
+                blob.set("advanced AUTO.ISW",1)
+                blob.set("advanced AUTO.ILP",1)
+                blob.set("advanced AUTO.IPS",1)
+            elif len(parameters) == 2:
+                blob.set("advanced AUTO.ISW",2)
+                blob.set("advanced AUTO.ILP",0)
+                blob.set("advanced AUTO.IPS",1)
+            else:
+                raise Exception("do not know how to do continuation of type '"+typ+"' in "+str(len(parameters)) + " parameters")
+        else:
+            raise Exception("do not know how to do continuation of type '+"+typ+"'")
+
+        return
+
+    # periodic from here on
+    if typ in ['lc','hb']:
         if len(parameters) != 1:
-            raise Exception("need exactly one parameter for steady state continuation")
+            raise Exception("need exactly one parameter for periodic orbit continuation from a Hopf bifurcation or limit cycle")
         blob.set("advanced AUTO.ISW",1)
         blob.set("advanced AUTO.ILP",1)
-        
-    elif typ in ['bp','lp','hb']:
-        if len(parameters) != 2:
-            raise Exception("need exactly two parameters for " + "Hopf" if typ =='hb' else "branch point" + " continuation")
+        blob.set("advanced AUTO.IPS",2)
+    elif typ in ['lclp', 'pd', 'tb']:
+        if len(parameters) !=2:
+            raise Exception("do not know how to do continuation of type '"+typ+"' in "+str(len(parameters))+" parameters")
         blob.set("advanced AUTO.ISW",2)
         blob.set("advanced AUTO.ILP",0)
+        blob.set("advanced AUTO.IPS",2)
+    else:
+        raise Exception("do not know how to do periodic orbit continuation of type '"+typ+"'")
 
 def prepare_auto_folder(oldPath, path):
     # guard against removing '/' or "../../usr/lib" and so on by some freak accident
@@ -57,7 +158,7 @@ def prepare_auto_folder(oldPath, path):
 lib.scigma_num_auto.restype=c_int
 lib.scigma_num_auto.argtypes=[c_char_p,c_int,c_int,c_int,POINTER(c_int),c_int,c_int,c_bool]
 
-def cont(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False):
+def cont(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False, periodic=False):
     win = windowlist.fetch(win)
 
     try:
@@ -80,19 +181,19 @@ def cont(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False):
     oldPath=None
 
     if g:
-        prepare_continuation(g,parameters,blob,win)
+        prepare_continuation(g,parameters,blob,periodic,win)
         oldPath=g['identifier'].rpartition('.')[0]
     elif win.selection:
-        prepare_continuation(win.selection,parameters,blob,win)
+        prepare_continuation(win.selection,parameters,blob,periodic,win)
         oldPath=win.selection['identifier'].rpartition('.')[0]
 
     if not path:
-        path=graphs.gen_ID("cont",win)
+        path=graphs.gen_ID("cycle" if periodic else "cont",win)
     prepare_auto_folder(oldPath,path)
 
-    cont={'identifier': path, 'npoints':nSteps,
-          'points':{'fp':0,'bp':0,'lp':0,'hb':0,'cp':0,'gh':0},
-          'branches':{'fp':0,'bp':0,'lp':0,'hb':0},
+    cont={'identifier': path, 'npoints':nSteps, 'periodic': periodic,
+          'points':{'fp':0,'lc':0,'bp':0,'lcbp':0,'lp':0,'lclp':0,'hb':0,'pd':0, 'tb':0, 'zh':0,'cp':0,'gh':0, '??':0},
+          'branches':{'fp':0,'lc':0,'bp':0,'lcbp':0,'lp':0,'lclp':0,'hb':0, 'pd':0,'tb':0},
           'timestamp':win.eqsys.timestamp()}
     cont['varying']=win.eqsys.var_names()+parameters
     cont['const']=win.eqsys.par_names()
@@ -105,8 +206,11 @@ def cont(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False):
     common.dict_enter(path,win.graphs,cont)
 
     identifier=create_string_buffer(bytes(path.encode("ascii")))
-    C_IntArrayType=c_int*len(parameters)
     indices=[win.eqsys.par_names().index(p) for p in parameters]
+    nparams = len(parameters) + 1 if periodic else len(parameters)
+    C_IntArrayType=c_int*(nparams)
+    if periodic: 
+        indices.append(11)
     indexArray=C_IntArrayType(*indices)
 
     cwd = os.getcwd()
@@ -116,11 +220,20 @@ def cont(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False):
     cont['taskID']=taskID
     os.chdir(cwd)
 
+def cycle(nSteps=1, parameters=None, g=None, path=None,win=None,noThread=False):
+    cont(nSteps,parameters,g,path,win,noThread,True)
+                            
 commands['co']=commands['con']=commands['cont']=cont
-
+commands['cy']=commands['cyc']=commands['cycl']=commands['cycle']=cycle
+                            
 def success(cont,win,args):
     if len(args)==0:
-        finalize_branch(cont)
+        # normally, an "SUCCESS" without arguments signals that we
+        # left a previous branch; due to a hopefully benign bug in
+        # auto.cpp, this is sometimes called even if we are just
+        # starting with the continuation => check for 'lastbranch'
+        if 'lastbranch' in cont:
+            finalize_branch(cont)
     elif len(args)==1:
         lib.scigma_num_finish_task(cont['taskID'])   
     else:
@@ -136,6 +249,7 @@ def success(cont,win,args):
         if typ==0:
             add_branch(cont,varWave,constWave,stab,brtype,win)
         else:
+            print("Punkt mit Label: "+ str(lab) + ", typ: "+str(typ))
             add_point(cont,varWave,constWave,stab, brtype if typ%10==9 else typ, lab, win)
 
 def minmax(cont):
@@ -159,7 +273,8 @@ def add_branch(cont,varWave,constWave,stab,typ,win):
 
     color = win.options['Drawing']['color']
 
-    typeName=AUTO_TYPES[typ]
+    typeName=auto_type(typ, cont['periodic'])
+
     cont['branches'][typeName]=cont['branches'][typeName]+1
     suffix = str(cont['branches'][typeName]).zfill(2)
     g={'identifier':cont['identifier']+'.'+typeName+'b'+suffix,'visible':False,
@@ -192,15 +307,15 @@ def finalize_branch(cont):
     del cont['lastbranch']
 
 def add_point(cont,varWave,constWave,stab,typ,lab,win):
-    if typ==0: # end point
+    if typ==0: # end point of steady state branch
         typ = 0 if stab <0 else 10
-        
-    pointStyle = POINT_STYLES[typ]
+
+    typeName=auto_type(typ, cont['periodic'])
+    pointStyle = point_style(typeName, stab<0)
+
     pointSize=win.options['Drawing']['marker']['size'].value
     color=win.options['Drawing']['color']
 
-    typeName=AUTO_TYPES[typ]
-    
     cont['points'][typeName]=cont['points'][typeName]+1
     suffix = str(cont['points'][typeName]).zfill(2)
 
@@ -209,11 +324,13 @@ def add_point(cont,varWave,constWave,stab,typ,lab,win):
        'varying':cont['varying'],'const':cont['const'],'timestamp':cont['timestamp'],
        'varwave':varWave,'constwave':constWave, 'autolabel':lab, 'autotype':typeName}
 
-    if typ == 9:
+    if typ == 0:
         g['stable']=True
     elif typ == 10:
         g['stable']=False
-    
+
+    print(pointStyle, pointSize)
+        
     g['callbacks']={'cleanup':None,'cursor':lambda point:graphs.stdcursor(g,point,win)}
 
     g['cgraph']=gui.Bundle(win.glWindow,1,1,
@@ -228,7 +345,8 @@ def add_point(cont,varWave,constWave,stab,typ,lab,win):
     common.dict_enter(g['identifier'],win.graphs,g)
     
     graphs.show(g,win)
-
+    picking.select(g['identifier'],-1,win,True)
+    
 def mouse_callback(g,double,button,point,x,y,win):
     identifier = g['identifier']
     if(double):
